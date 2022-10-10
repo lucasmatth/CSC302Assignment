8fd28d7df974b5137b231f38c26d3e8dfe867037
lucasmatth <lucas.matthews@mail.utoronto.ca>
2022-10-10 01:12:34 -0400

    added testing suite

--- mysite/mainapp/tests.py
+++ mysite/mainapp/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
-
+from django.test import TestCase, RequestFactory
+from .views import *
 # Create your tests here.
+
+class FirstTest(TestCase):
+    def setUp(self):
+        self._factory = RequestFactory()
+    def test_first(self):
+        request = self._factory.get("/mainapp/")
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
