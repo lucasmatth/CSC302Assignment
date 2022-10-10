9d49b146711220f200989b6c06e8764705d47e11
lucasmatth <lucas.matthews@mail.utoronto.ca>
2022-10-09 23:16:50 -0400

    updated landing page to say line in handout

--- mysite/mainapp/views.py
+++ mysite/mainapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("HELLO WORLD")
+    return HttpResponse("I am running! I'm gonna talk to a database!")
