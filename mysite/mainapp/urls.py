3f6ee3871d467592c403fbcf20a5112fb20ba43d
lucasmatth <lucas.matthews@mail.utoronto.ca>
2022-11-16 13:51:11 -0500

    fixed minor bug stopping program from running

--- mysite/mainapp/urls.py
+++ mysite/mainapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
-    path("date_til_trend/", views,date_til_trend, name='date_til_trend')
+    path("", views.index, name='index'),
+    path("date_til_trend/", views.date_til_trend, name='date_til_trend')
     ]
