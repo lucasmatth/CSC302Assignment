be671418cb54321d351af26bd85d58462460f493
Curtain64 <bryanbui2504@gmail.com>
2022-12-06 13:19:06 -0500

    Edit Frontend Add Data

--- mysite/mainapp/urls.py
+++ mysite/mainapp/urls.py
@@ -4,7 +4,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("first_data/", views.first_data, name='first_data'),
-    path("date_til_trend/", views.date_til_trend, name='date_til_trend')
+    path("day/", views.first_data, name='first_data'),
+    path("hour/", views.second_data, name = 'second_data'),
+    path("view/", views.third_data, name = "third_data"),
+    path("date_til_trend/", views.date_til_trend, name='date_til_trend'),
     ]
 urlpatterns += staticfiles_urlpatterns()
