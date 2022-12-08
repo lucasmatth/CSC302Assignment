149c6c60a2c694ad1f8a48d80f7a4d3ee413ae17
lucasmatth <lucas.matthews@mail.utoronto.ca>
2022-12-08 16:35:26 -0500

    added some stuff

--- data analysis/hours by other stats/hours by others.py
+++ data analysis/hours by other stats/hours by others.py
@@ -9,6 +9,10 @@ def calc_mean_byday(vid_list_by_day, which):
         for vid in vid_list_by_day[ele]:
             if which == "views":
                 total += vid[2]
+            if which == 'likes':
+                total += vid[3]
+            if which == 'comments':
+                total += vid[4]
         total = total / len(vid_list_by_day[ele])
         all_totals.append(total)
     return all_totals
@@ -20,6 +24,10 @@ def calc_tot_by_hour(hours_dic_vids, which):
         for vid in hours_dic_vids[ele]:
             if which == 'views':
                 total += vid[2]
+            if which == 'likes':
+                total += vid[3]
+            if which == 'comments':
+                total += vid[4]
         all_totals.append(total)
     return all_totals
 
@@ -27,7 +35,7 @@ con = sqlite3.connect("../../videodata.db")
 cur = con.cursor()
 
 data = con.execute('''SELECT
-id, publish_time, views
+id, publish_time, views, likes, comment_counter
 FROM videodata''').fetchall()
 con.close()
 
@@ -70,15 +78,63 @@ with open('total_views_by_hour.json', 'w', encoding='utf-8') as f:
     json.dump(dict(zip(list(hours_dic_num.keys()), total_views_by_hour)), f)
 plt.show()
 
-mean_byday_views = calc_mean_byday(hours_dic_vids, "views")
+mean_views_by_hour = calc_mean_byday(hours_dic_vids, "views")
 plt.figure(figsize=(40,10))
 plt.tick_params(axis='both', labelsize=9)
 plt.xticks(rotation=50)
 plt.xlabel("Hours of day", fontsize=16)
 plt.ylabel("Mean number of views", fontsize=16)
-plt.bar([str(x) for x in list(hours_dic_num.keys())], mean_byday_views, width=1, color='#800000')
+plt.bar([str(x) for x in list(hours_dic_num.keys())], mean_views_by_hour, width=1, color='#800000')
 plt.savefig('mean_views_by_hour.png')
 with open('mean_views_by_hour.json', 'w', encoding='utf-8') as f:
-    json.dump(dict(zip(list(hours_dic_num.keys()), mean_byday_views)), f)
+    json.dump(dict(zip(list(hours_dic_num.keys()), mean_views_by_hour)), f)
+plt.show()
+
+total_likes_by_hour = calc_tot_by_hour(hours_dic_vids, 'likes')
+plt.figure(figsize=(40,10))
+plt.tick_params(axis='both', labelsize=9)
+plt.xticks(rotation=50)
+plt.xlabel('Hour of day', fontsize=16)
+plt.ylabel('Total likes', fontsize=16)
+plt.bar([str(x) for x in list(hours_dic_num.keys())], total_likes_by_hour, width=1, color='#000080')
+plt.savefig('total_likes_by_hour.png')
+with open('total_likes_by_hour.json', 'w', encoding='utf-8') as f:
+    json.dump(dict(zip(list(hours_dic_num.keys()), total_likes_by_hour)), f)
+plt.show()
+
+mean_likes_by_hour = calc_mean_byday(hours_dic_vids, "likes")
+plt.figure(figsize=(40,10))
+plt.tick_params(axis='both', labelsize=9)
+plt.xticks(rotation=50)
+plt.xlabel("Hours of day", fontsize=16)
+plt.ylabel("Mean number of likes", fontsize=16)
+plt.bar([str(x) for x in list(hours_dic_num.keys())], mean_likes_by_hour, width=1, color='#800000')
+plt.savefig('mean_likes_by_hour.png')
+with open('mean_likes_by_hour.json', 'w', encoding='utf-8') as f:
+    json.dump(dict(zip(list(hours_dic_num.keys()), mean_likes_by_hour)), f)
+plt.show()
+
+total_comments_by_hour = calc_tot_by_hour(hours_dic_vids, 'comments')
+plt.figure(figsize=(40,10))
+plt.tick_params(axis='both', labelsize=9)
+plt.xticks(rotation=50)
+plt.xlabel('Hour of day', fontsize=16)
+plt.ylabel('Total comments', fontsize=16)
+plt.bar([str(x) for x in list(hours_dic_num.keys())], total_comments_by_hour, width=1, color='#000080')
+plt.savefig('total_comments_by_hour.png')
+with open('total_comments_by_hour.json', 'w', encoding='utf-8') as f:
+    json.dump(dict(zip(list(hours_dic_num.keys()), total_comments_by_hour)), f)
+plt.show()
+
+mean_comments_by_hour = calc_mean_byday(hours_dic_vids, "comments")
+plt.figure(figsize=(40,10))
+plt.tick_params(axis='both', labelsize=9)
+plt.xticks(rotation=50)
+plt.xlabel("Hours of day", fontsize=16)
+plt.ylabel("Mean number of comments", fontsize=16)
+plt.bar([str(x) for x in list(hours_dic_num.keys())], mean_comments_by_hour, width=1, color='#800000')
+plt.savefig('mean_comments_by_hour.png')
+with open('mean_comments_by_hour.json', 'w', encoding='utf-8') as f:
+    json.dump(dict(zip(list(hours_dic_num.keys()), mean_comments_by_hour)), f)
 plt.show()
 
